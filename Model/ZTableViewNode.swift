978cb17ef6a1abc98f1d003069c31377f8572644
minster <zominster@gmail.com>
2024-05-19 23:41:46 +0800

    add model

---
+++ Model/ZTableViewNode.swift
@@ -0,0 +1,12 @@
+//
+//  ZTableViewNode.swift
+//  ZTableView
+//
+//  Created by 赖依娴 on 2024/5/19.
+//
+
+import UIKit
+
+class ZTableViewNode: NSObject {
+
+}
---
+++ Model/ZTableViewSection.swift
@@ -0,0 +1,12 @@
+//
+//  ZTableViewSection.swift
+//  ZTableView
+//
+//  Created by 赖依娴 on 2024/5/19.
+//
+
+import UIKit
+
+class ZTableViewSection: NSObject {
+
+}
